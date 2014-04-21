977af7eba74b26f46a56f69675c3ea0ae63b9e2b
lkg0dzre <lkg0dzre@gmail.com>
2014-04-21 16:32:19 +0700

    Lesson 15 begin

--- run.rb
+++ run.rb
@@ -1,15 +1,20 @@
-class Item
-  attr_reader :name
-
-  def initialize(name)
-    @id, @name = 1, name
+class Student
+  def initialize name, born_at, gender
+    @name = name
+    @born_at = born_at
+    @gender = gender
   end
 
+  def age
+    age Date.today.year - @born_at.year
+    age -= 1 if Date.today < birthday + age.years
+  end
 end
 
-class SpecialItem < Item
-  def initialize name, special_attributes
-    super name
-    @special_attributes = special_attributes
+class Teacher
+  def initialize name, proficiency, list_of_grades
+    @name = name
+    @proficiency = proficiency
+    @list_of_grades = list_of_grades
   end
 end
