efb29d7e9427c2b0b167a12d9ca3ee0d71550337
lkg0dzre <lkg0dzre@gmail.com>
2014-04-21 16:43:54 +0700

    Lesson 15 mixin

--- run.rb
+++ run.rb
@@ -1,10 +1,8 @@
-class User
-  def initialize username, password
-    @username = username
-    @password = password
-  end
-
-  def login
+module Login
+  def login username, password
+    @username == username and
+    @password == password and
+    p "Successfully logged in as #@username"
 
   end
 
@@ -17,11 +15,14 @@ class User
   end
 end
 
-class Student < User
-  def initialize name, born_at, gender
+class Student
+  include Login
+    def initialize name, born_at, gender, username, password
     @name = name
     @born_at = born_at
     @gender = gender
+    @username = username
+    @password = password
   end
 
   def age
@@ -30,10 +31,20 @@ class Student < User
   end
 end
 
-class Teacher < User
-  def initialize name, proficiency, list_of_grades
+class Teacher
+  include Login
+  def initialize name, proficiency, list_of_grades, username, password
     @name = name
     @proficiency = proficiency
     @list_of_grades = list_of_grades
+    @username = username
+    @password = password
   end
 end
+
+
+student = Student.new "Bob", Time.new("1970-04-12"), "M", "bob", "alice"
+teacher = Teacher.new "Alice", Time.new("1970-04-12"), "F", "alice", "tony"
+
+student.login "bob", "alice"
+teacher.login "alice", "charlie"
