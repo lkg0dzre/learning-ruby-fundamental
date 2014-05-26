c26692e765ac748e02690c5ac3c06cb3ced54ec8
lkg0dzre <lkg0dzre@gmail.com>
2014-05-26 11:21:15 +0400

    ARGV

--- run.rb
+++ run.rb
@@ -1,18 +1,5 @@
-def median(*list)
-  return nil if list.empty?
-  list.sort!
+p ARGV
 
-  count = list.count
-
-  if count.odd?
-    n = (count - 1) / 2
-    list[n]
-  elsif
-    n1 = count / 2
-    n2 = n1 - 1
-    (list[n1] + list[n2]) / 2.0
-  end
-end
-
-p median 2,3,1
-p median 1,2,3,4
+puts ARGV[0]
+puts ARGV[1]
+puts ARGV[2]
